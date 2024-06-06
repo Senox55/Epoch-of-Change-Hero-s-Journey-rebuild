b8c24a289cd41dc710669499333c8450aaf9f0ca
Senox <blya>
2024-06-06 06:49:59 +0500

    fix bag with tree and bush

--- data/tilesets/plants.tsx
+++ data/tilesets/plants.tsx
@@ -2,9 +2,9 @@
 <tileset version="1.10" tiledversion="1.10.2" name="plants" tilewidth="113" tileheight="139" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="113" height="139" source="../../../../tree.png"/>
+  <image width="113" height="139" source="../../assets/world/tree.png"/>
  </tile>
  <tile id="1">
-  <image width="47" height="42" source="../../../../bush.png"/>
+  <image width="47" height="42" source="../../assets/world/bush.png"/>
  </tile>
 </tileset>
